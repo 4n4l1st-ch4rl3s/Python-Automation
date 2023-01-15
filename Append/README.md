34f84916db7a22ae63c4ceab6ed5c0a09d40f9af
4n4l1st-ch4rl3s <63286909+4n4l1st-ch4rl3s@users.noreply.github.com>
2023-01-15 13:53:13 +0300

    First commit

---
+++ Append/README.md
@@ -0,0 +1,16 @@
+# README.md
+
+## Append.py
+
+This is a python script that appends a digit at the beggining of a number that is in a csv file and outputs it to `output.csv`.
+The script uses multi threading to make the process fast.
+
+## Usage
+
+Clone the repository
+install the dependencies
+>pip3 install -r requirements.txt
+
+### NOTE
+
+Edit the csv file that is being used to get the numbers.
---
+++ Append/append.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import csv
+import threading
+
+#create a function that read the csv file and outputs the new_numbers to a new csv.
+def process_file():
+    #opens and reads the file
+    with open("contact.csv", "r") as csvfile:
+        csvFile = csv.reader(csvfile)
+        # the new_row will contain the aappended value
+        new_rows = [['254'+row[0].replace(',','')] + row[1:] for row in csvFile]
+    
+    #creates the output csv to which the new_rows will be saved.
+    with open("output.csv", "w", newline='') as outputfile:
+        writer = csv.writer(outputfile)
+        writer.writerows(new_rows)
+
+#initiaslizing the thread
+t = threading.Thread(target=process_file)
+#start the threads
+t.start()
+
+
